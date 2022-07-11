dbfc56a0d3bf4338d385368cafe25352d981919e
Jin-Hyeok, Cha <38489680+gignac-cha@users.noreply.github.com>
2022-07-12 00:07:47 +0900

    add button listener

--- script.ts
+++ script.ts
@@ -1,5 +1,10 @@
 window.addEventListener('load', (ev: Event) => {
   const button: HTMLButtonElement = document.createElement('button');
   button.textContent = 'Click!';
+  button.addEventListener('click', (ev: MouseEvent) => {
+    const div: HTMLDivElement = document.createElement('div');
+    div.textContent = `${new Date()}`;
+    document.body.appendChild(div);
+  });
   document.body.appendChild(button);
 });
