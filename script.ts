26ec8e2f5325d28b829af288294ffa0bf5f26862
Jin-Hyeok, Cha <38489680+gignac-cha@users.noreply.github.com>
2022-07-11 13:30:10 +0000

    add page

---
+++ script.ts
@@ -0,0 +1,5 @@
+window.addEventListener('load', (ev: Event) => {
+  const button: HTMLButtonElement = document.createElement('button');
+  button.textContent = 'Click!';
+  document.body.appendChild(button);
+});
